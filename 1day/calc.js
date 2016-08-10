96af57479ce7af8750f870d35b505331a0324831
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-08-10 18:51:01 +0300

    do it simple

--- 1day/calc.js
+++ 1day/calc.js
@@ -1,17 +1,15 @@
 for (var i = 0; i < data.length; i++) {
-	if(typeof data[i] != "number" && typeof data[i] != "string"){
-		continue;
-	}
-	if(data[i] == 0){
-		data[i] = +data[i] + 10;
-		continue;
-	}
-	if(data[i] > 100){
-		data[i] = +data[i] - 100;
-		continue;
-	}
-	if(data[i] < 100){
-		data[i] = +data[i].valueOf() + 100;
-		continue;
+	if(data[i] != null){		
+		if(data[i] < 100){
+			if(data[i] == 0)
+				data[i] = +data[i] + 10;
+			else
+				data[i] = +data[i] + 100;
+		}
+		
+		else if(data[i] > 100){
+			data[i] = +data[i] - 100;
+			continue;
+		}
 	}
 }
--- 1day/log.js
+++ 1day/log.js
@@ -2,11 +2,10 @@
 for (var i = 0; i < data.length; i++) {
 	if(data[i] === undefined){
 		console.log("не определено");
-		continue;
 	}
-	if(data[i] === null){
+	
+	 else if(data[i] === null){
 		console.log("Не указано");
-		continue;
 	}
 	console.log("data[" + i + "]" + "=" +data[i]);
 }
