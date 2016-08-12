52949ad2fa57facffcd68bf76ab724a0d4ab1982
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-08-12 12:25:08 +0300

    done task 2

--- 2day/init.js
+++ 2day/init.js
@@ -2,26 +2,14 @@
 var array = [];
 var type;
 
-function getCount(){
-	return this.count;
-}
-
 for (var i = 0; i < 5; i++) {
 	array[i] = {};
 	array[i].count = random(1,10);
-	switch (random(1,3)){
-		case 1:
-			array[i].getCount1 = getCount;
-			type = 1;
-			break;
-		case 2:
-			array[i].getCount2 = getCount;
-			type = 2;
-			break;
-		case 3:
-			array[i].getCount2 = getCount;
-			type = 3;
-			break;
+	type = random(1,3);
+	array[i]["getCount" + type] = function(){
+		return this.count;
 	}
-	console.log("type=" + type + ", count=" + array[i].count);
+	console.log("type=%d, count=%d", type, array[i].count);
 }
+
+log();
--- 2day/log.js
+++ 2day/log.js
@@ -1,6 +1,15 @@
 
-var result = [];
+var result = [0,0,0];
 
-for (var i = 0; i < array.length; i++) {
-	array[i]
+var func;
+function log(){
+	for (var i = 0; i < array.length; i++) {
+		result[0] += array[i].getCount1 && array[i].getCount1() || 0;
+		result[1] += array[i].getCount2 && array[i].getCount2() || 0;
+		result[2] += array[i].getCount3 && array[i].getCount3() || 0;   
+	}
+
+	for (var i = 0; i < result.length; i++) {
+	console.log("count%d=%d", i+1, result[i]);
+}
 }
