beb8bb83365e503faa98a29f409417f4d6f6c4a6
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-08-16 11:25:04 +0300

    remade 3 task

--- 3day/main.js
+++ 3day/main.js
@@ -1,50 +1,54 @@
-$(document).ready(function()
+$(function()
 {
 	$("#Generate").on("click", generate);
 	$("#SetColor").on("click", setColor);
 	$("#Reset").on("click", reset);
-})
 
-function random(min, max) {
-	return Math.floor((Math.random() * max) + min);
-}
-
-function getColor(number)
-{
-	if(number > 75)
-		return "#f44336";
-	if (number > 50)
-		return "#ff9800";
-	if(number > 25)
-		return "#4caf50";
-	return "white";
-}
-
-function generate()
-{
-	for (var i = 0; i < 50; i++) {
-		var $obj = $("<div></div>");
-		$obj.text(random(0,100));
-		$obj.addClass("item");
-		$(".field").append($obj);
+	function random(min, max){
+		return Math.floor((Math.random() * max) + min);
 	}
-	$("#Reset").prop("disabled",false);
-	$("#SetColor").prop("disabled",false);
-	$(this).prop("disabled",true);
-}
 
-function setColor()
-{
-	$(".item").each(function()
-	{
-		$(this).css("background-color", getColor($(this).text()));
-	})
-	$(this).prop("disabled",true);
-}
+	function getColor(number){
+		if(number > 75)
+			return "red";
+		if (number > 50)
+			return "orange";
+		if(number > 25)
+			return "green";
+		return "white";
+	}
 
-function reset(){
-	$(".item").remove();
-	$("#Generate").prop("disabled",false);
-	$("#SetColor").prop("disabled",true);
-	$(this).prop("disabled",true);
-}
+	function generate(){
+		if(!$(this).hasClass("disabled")){
+			for (var i = 0; i < 50; i++){
+				var $obj = $("<div></div>");
+				$obj.text(random(0,100));
+				$obj.addClass("item");
+				$(".field").append($obj);
+			}
+			$("#Reset").removeClass("disabled");
+			$("#SetColor").removeClass("disabled");
+			$(this).addClass("disabled");
+		}
+	}
+
+	function setColor(){
+		if(!$(this).hasClass("disabled")){
+			$(".item").each(function()
+			{
+				$(this).addClass(getColor($(this).text()));
+			})
+			$(this).addClass("disabled");
+		}
+	}
+
+	function reset(){
+		if(!$(this).hasClass("disabled")){
+			$(".item").remove();
+			$("#Generate").removeClass("disabled");
+			$("#SetColor").addClass("disabled");
+			$(this).addClass("disabled");
+		}
+	}
+
+})
