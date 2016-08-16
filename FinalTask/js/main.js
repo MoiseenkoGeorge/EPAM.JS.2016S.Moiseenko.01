f3254ffaf864508926282174523c6b4c8a7a3655
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-08-16 15:21:58 +0300

    add some functionality

--- FinalTask/js/main.js
+++ FinalTask/js/main.js
@@ -1,10 +1,29 @@
-$(document).ready(function(){
-	$(Btn).on("click",clickButton)
-})
+$(function(){
+	$("#Btn").on("click",clickButton)
 
-function clickButton()
-{
-	$(this).prop("value","stop");
-	$(this).removeClass("btn-success");
-	$(this).addClass("btn-danger");
+	var items = ["cheese","orange","cherry","pumpkin","bomb"]
+
+	function random(min, max){
+		return Math.floor((Math.random() * max) + min);
+	}
+
+	function clickButton(){
+	var $this = $(this);
+	if($this.hasClass("btn-success")){
+		$this.prop("value","stop");
+		$this.removeClass("btn-success");
+		$this.addClass("btn-danger");
+	}
+	else{
+		$this.prop("value","start");
+		$this.removeClass("btn-danger");
+		$this.addClass("btn-success");
+	}
+
+	function start(){
+		while(true){
+			var $obj = $("<div></div>");
+		}
+	}
 }
+});
